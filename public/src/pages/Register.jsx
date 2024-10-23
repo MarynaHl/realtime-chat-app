a8d8851ee7211d82606f1b26503f86a47b22c11d
Maryna Hlushak <hlushak.m.v@gmail.com>
2024-10-23 22:23:39 +0100

    Fix: corrected styles and layout issues in Register component

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import Logo from "../assets/logo.svg"
+import Logo from "../assets/logo.svg";
 
 function Register() {
     const handleSubmit = (event) => {
@@ -19,78 +19,83 @@ function Register() {
                 <img src={Logo} alt='Logo' />
                 <h1>snappy</h1>
             </div>
-                  <input
-                      type="email"
-                      placeholder="Email"
-                      name="email"
-                      onChange={(e) => handleChange(e)}
-                  />
-                <input
-                      type="password"
-                      placeholder="Password"
-                      name="password"
-                      onChange={(e) => handleChange(e)}
-                  />
-                <input
-                      type="password"
-                      placeholder="Confirm Password"
-                      name="confirmPassword"
-                      onChange={(e) => handleChange(e)}
-                  />
-                  <button type="submit">Create User</button>
-                  <span>
-                      Already have an account? <Link to="/login">Login</Link>
-                  </span>
+            <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                onChange={(e) => handleChange(e)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={(e) => handleChange(e)}
+            />
+            <input
+                type="password"
+                placeholder="Confirm Password"
+                name="confirmPassword"
+                onChange={(e) => handleChange(e)}
+            />
+            <button type="submit">Create User</button>
+            <span>
+                Already have an account? <Link to="/login">Login</Link>
+            </span>
         </form>
     </FormContainer>
     </>
-  )
+  );
 }
 
 const FormContainer = styled.div`
-height: 100vh;
-width: 100vw;
-display: flex;
-flex-direction: column;
-justify-content: center;
-gap: 1rem;
-align-items: center;
-background-color: #131324;
-.brand {
-display: flex;
-align-items: center;
-gap: 1rem;
-justify-content: center;
-img {
-height: 5rem;
-}
-h1 {
-color: white;
-text-transform: uppercase;
-}
-}
-form {
-display: flex;
-flex-direction: column;
-gap: 2rem;
-background-color: 00000076;
-border-radius: 2rem;
-padding: 3rem 5rem;
-input {
-background-color: transparant;
-padding 1rem;
-border: 0.1rem solid #4e0eff;
-border-radius: 0.4rem;
-color: white;
-width: 100%;
-font-size: 1rem;
-&: focus{
-border: 0.1rem solid #997af0
-outline: none;
-}
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
 
-}
-}
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    
+    img {
+      height: 5rem;
+    }
+    
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      color: white;
+      width: 100%;
+      font-size: 1rem;
+
+      &:focus {
+        border: 0.1rem solid #997af0;
+        outline: none;
+      }
+    }
+  }
 `;
 
-export default Register
+export default Register;
