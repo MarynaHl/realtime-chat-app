bec499d6ba8c1429b3f7646e5635c41862846112
Maryna Hlushak <hlushak.m.v@gmail.com>
2024-10-23 22:32:20 +0100

    upd

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -17,7 +17,7 @@ function Register() {
         <form onSubmit={(event) => handleSubmit(event)}>
             <div className='brand'>
                 <img src={Logo} alt='Logo' />
-                <h1>snappy</h1>
+                <h1>whisper</h1>
             </div>
             <input
                 type="email"
