79fbda4efe37a308ca00c1301a34f829c9d23998
Maryna Hlushak <hlushak.m.v@gmail.com>
2024-10-27 21:50:05 +0000

    add loader

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import Loader from "../assets/loader.svg";
+import Loader from "../assets/loader.gif";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import { setAvatarRoute } from '../utils/APIRouters';
 
 export default function SetAvatar() {
+
+    const api = "https://api.multiavatar.com/45678945";
+    const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Set Avatar</h1>
-    </div>
+    <>
+    <Container><h1>Set Avatar</h1></Container>
+    <ToastContainer />
+    </>
   );
 }
+
+const Container = styled.div``;
