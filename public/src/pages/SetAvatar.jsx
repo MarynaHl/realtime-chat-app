3ced8eb8b45d41ce394506724fa431285aa926ee
Maryna Hlushak <hlushak.m.v@gmail.com>
2024-10-27 21:19:21 +0000

    add route for setAvatar

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import Loader from "../assets/loader.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { setAvatarRoute } from '../utils/APIRouters';
 
 export default function SetAvatar() {
   return (
