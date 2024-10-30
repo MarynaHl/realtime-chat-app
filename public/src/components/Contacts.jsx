b27f59874ca096931a4c5e3e26a47af13b5a7901
Maryna Hlushak <hlushak.m.v@gmail.com>
2024-10-30 14:42:51 +0000

    upd

--- public/src/components/Contacts.jsx
+++ public/src/components/Contacts.jsx
@@ -2,5 +2,19 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Logo from "../assets/logo.svg";
 
+export default function Contacts({contacts, currentUser}) {
+    const [currentUserName, setCurrentUserName] = useState(undefined);
+    const [currentUserImage, setCurrentUserImage] = useState(undefined);
+    const [currentSelected, setCurrentSelected] = useState(undefined);
+    useEffect(()=>{
+        if(currentUser) {
+            setCurrentUserImage(currentUser.avatarImage);
+            setCurrentUserName(currentUser.username);
+        }
+    },[currentUser]);
+    const changeCurrentChat = (index, contact) => {
 
+    }
+  return <div>Contacts</div>
 }
+
