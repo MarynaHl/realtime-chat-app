84b939e6446e4d4ea9bae4f04c14904447130069
Maryna Hlushak <hlushak.m.v@gmail.com>
2024-10-30 13:48:50 +0000

    Fix async calls in Chat component and improve CSS structure

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -7,53 +7,61 @@ import { allUsersRoute } from '../utils/APIRouters';
 function Chat() {
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
-  const [currentUser, setCurrentUser] = useState(undefined)
-  useEffect(async()=>{
-    if (!localStorage.getItem('chat-app-user')) {
-      navigate('/login');
-    } else {
-setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")))
-    }  
-  },[])
-  useEffect(async()=> {
-    if(currentUser) {
-      if(currentUser.isAvatarImageSet) {
-        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`)
-        setContacts(data.data);
-      } else {
-        navigate("/setAvatar");
-      }
-    }
-  },[currentUser])
-  return (
-  <Container>
-  
-    <div className='container'>Chat</div>
+  const [currentUser, setCurrentUser] = useState(undefined);
 
-  </Container>
-    );
+  useEffect(() => {
+    const checkUser = async () => {
+      if (!localStorage.getItem('chat-app-user')) {
+        navigate('/login');
+      } else {
+        setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+      }
+    };
+    checkUser();
+  }, [navigate]);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const { data } = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data);
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    };
+    fetchContacts();
+  }, [currentUser, navigate]);
+
+  return (
+    <Container>
+      <div className="container">Chat</div>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-height: 100vh;
-width: 100vw;
-display: flex;
-flex-direction: column;
-justify-content: center;
-gap: 1rem;
-align-items: center;
-background-color: #131324;
-.container {
-height: 85vh;
-width: 85vw;
-background-color: #00000076;
-display: grid;
-grid-template-columns: 25% 75%;
-@media screen and (min-width:720px) and (max-width:1080px) {
-grid-template-columns: 35% 65%;
-}
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
 
-}
-`
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
 
-export default Chat
+export default Chat;
