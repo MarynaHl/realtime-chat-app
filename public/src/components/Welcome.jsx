366c3ba5846ce208083b1bf5a9d7c096bc2b3b8e
Maryna Hlushak <hlushak.m.v@gmail.com>
2024-11-02 21:24:32 +0000

    add style

--- public/src/components/Welcome.jsx
+++ public/src/components/Welcome.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
-import Robot from "../assets"
+import Robot from "../assets/robot.gif"
 export default function Welcome ({ currentUser }){
-    return <div>Welcome</div>;
+    return <Container>
+<img src={Robot} alt="Robot" />
+<h1>
+    Welcome, <span>{currentUser.username}!</span>
+</h1>
+<h3>Please select a chat to Start Messaging</h3>
+    </Container>;
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
