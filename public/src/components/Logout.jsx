acc07a4bcde7a15cacc929cac1290fd198ba7c23
Maryna Hlushak <hlushak.m.v@gmail.com>
2024-11-04 21:26:11 +0000

    add style

--- public/src/components/Logout.jsx
+++ public/src/components/Logout.jsx
@@ -18,4 +18,17 @@ export default function Logout() {
   );
 }
 
-const Button = styled.button``;
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: #9a86f3;
+  border: none;
+  cursor: pointer;
+  svg {
+    font-size: 1.3rem;
+    color: #ebe7ff;
+  }
+`;
