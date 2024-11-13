2ebe2d439157cd1efa616af83466e0bc34484f25
Maryna Hlushak <hlushak.m.v@gmail.com>
2024-11-13 20:35:17 +0000

    fix bugs

--- public/src/components/ChatInput.jsx
+++ public/src/components/ChatInput.jsx
@@ -4,25 +4,18 @@ import { IoMdSend } from "react-icons/io";
 import styled from "styled-components";
 import Picker from "emoji-picker-react";
 
-export default function ChatInput({ handleSendMsg }) {
-  const [msg, setMsg] = useState("");
+export default function ChatInput() {
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [msg, setMsg] = useState("");
+
   const handleEmojiPickerhideShow = () => {
     setShowEmojiPicker(!showEmojiPicker);
   };
 
-  const handleEmojiClick = (event, emojiObject) => {
-    let message = msg;
-    message += emojiObject.emoji;
-    setMsg(message);
-  };
-
-  const sendChat = (event) => {
-    event.preventDefault();
-    if (msg.length > 0) {
-      handleSendMsg(msg);
-      setMsg("");
-    }
+  const handleEmojiClick = (event, emoji) => {
+let message = msg;
+message += emoji.emoji;
+setMsg(message);
   };
 
   return (
@@ -33,14 +26,13 @@ export default function ChatInput({ handleSendMsg }) {
           {showEmojiPicker && <Picker onEmojiClick={handleEmojiClick} />}
         </div>
       </div>
-      <form className="input-container" onSubmit={(event) => sendChat(event)}>
+      <form className="input-container">
         <input
           type="text"
-          placeholder="type your message here"
-          onChange={(e) => setMsg(e.target.value)}
+          placeholder="Message..." />
           value={msg}
-        />
-        <button type="submit">
+          onChange={(e)=>setMsg(e.target.value)}
+        <button className="submit">
           <IoMdSend />
         </button>
       </form>
@@ -51,93 +43,96 @@ export default function ChatInput({ handleSendMsg }) {
 const Container = styled.div`
   display: grid;
   align-items: center;
-  grid-template-columns: 5% 95%;
-  background-color: #080420;
-  padding: 0 2rem;
-  @media screen and (min-width: 720px) and (max-width: 1080px) {
-    padding: 0 1rem;
-    gap: 1rem;
-  }
+  padding: 1rem;
+  background-color: #2b2b41;
+  position: relative;
+
   .button-container {
     display: flex;
     align-items: center;
     color: white;
-    gap: 1rem;
+    margin-right: 1rem;
+
     .emoji {
       position: relative;
+
       svg {
         font-size: 1.5rem;
-        color: #ffff00c8;
+        color: #f1c40f;
         cursor: pointer;
       }
+
       .emoji-picker-react {
         position: absolute;
-        top: -350px;
-        background-color: #080420;
-        box-shadow: 0 5px 10px #9a86f3;
-        border-color: #9a86f3;
+        bottom: 50px;
+        background-color: #27293d;
+        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+        border-color: #44475a;
+        border-radius: 0.5rem;
+
         .emoji-scroll-wrapper::-webkit-scrollbar {
-          background-color: #080420;
+          background-color: #27293d;
           width: 5px;
-          &-thumb {
-            background-color: #9a86f3;
-          }
         }
-        .emoji-categories {
-          button {
-            filter: contrast(0);
-          }
+
+        .emoji-scroll-wrapper::-webkit-scrollbar-thumb {
+          background-color: #44475a;
         }
+
+        .emoji-categories button {
+          filter: contrast(0);
+        }
+
         .emoji-search {
-          background-color: transparent;
-          border-color: #9a86f3;
+          background-color: #44475a;
+          border-color: #f1c40f;
         }
+
         .emoji-group:before {
-          background-color: #080420;
+          background-color: #27293d;
         }
       }
     }
   }
+
   .input-container {
-    width: 100%;
-    border-radius: 2rem;
     display: flex;
     align-items: center;
-    gap: 2rem;
-    background-color: #ffffff34;
-    input {
-      width: 90%;
-      height: 60%;
-      background-color: transparent;
-      color: white;
-      border: none;
-      padding-left: 1rem;
-      font-size: 1.2rem;
+    background-color: #44475a;
+    border-radius: 1.5rem;
+    padding: 0.5rem 1rem;
+    width: 100%;
 
-      &::selection {
-        background-color: #9a86f3;
+    input {
+      flex: 1;
+      background-color: transparent;
+      border: none;
+      color: #f8f8f2;
+      font-size: 1rem;
+      padding-left: 0.5rem;
+
+      &::placeholder {
+        color: #8d8d8d;
       }
+
       &:focus {
         outline: none;
       }
     }
+
     button {
-      padding: 0.3rem 2rem;
-      border-radius: 2rem;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: #9a86f3;
+      background-color: #f1c40f;
       border: none;
-      @media screen and (min-width: 720px) and (max-width: 1080px) {
-        padding: 0.3rem 1rem;
-        svg {
-          font-size: 1rem;
-        }
-      }
+      border-radius: 50%;
+      padding: 0.5rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+
       svg {
-        font-size: 2rem;
-        color: white;
+        font-size: 1.2rem;
+        color: #2b2b41;
       }
     }
   }
